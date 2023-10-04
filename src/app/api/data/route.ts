b6cb627cf171a4afd976d0cfc81c9cfac94a3930
Christian Foidl <christian@wunderwerk.io>
2023-10-04 08:24:09 +0200

    fix: disable fetch cache

--- src/app/api/data/route.ts
+++ src/app/api/data/route.ts
@@ -4,4 +4,7 @@ export const GET = async () => {
   const data = await fetchWeatherData();
 
   return new Response(JSON.stringify(data), { status: 200 });
-}
+};
+
+export const fetchCache = "force-no-store";
+
